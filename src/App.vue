82907cdaa744fcaa215ebac4dc48c20cf986ef0b
gianluigiricciolio <gianluigiricciolio@proton.me>
2024-05-29 17:47:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+  export default {
+    name:'myapp',
+    data(){
+      return{
+        title: 'La mia prima app con vite'
+      }
+    }
+
+  }
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+
+<style>
+  h1{
+    color:gold
+  }
+</style>
